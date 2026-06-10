80b815df55d9a3f781f195a9df949fd96e23e8a3
User <user@example.com>
2026-06-10 19:12:05 +0700

    chore: add history.pl initial data

---
+++ history.pl
@@ -0,0 +1,8 @@
+[
+    {
+        "id": "3N3ZwIB_X4Y",
+        "title": "\ud83d\udd34 LIVE Crystal Bay Yacht Club Lamai | Koh Samui Beach Webcam | Thailand 24/7 | 2160p 4K",
+        "channel": "Unknown Channel",
+        "thumbnail": "https://img.youtube.com/vi/3N3ZwIB_X4Y/mqdefault.jpg"
+    }
+]
